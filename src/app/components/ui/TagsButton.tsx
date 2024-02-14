6d33f898a09a890f97fbe6fc794f6e3597968d99
phantacia <patriceeandrea@gmail.com>
2024-02-14 11:55:38 +0700

    fixed css setting for improved layout

--- src/app/components/ui/TagsButton.tsx
+++ src/app/components/ui/TagsButton.tsx
@@ -1,7 +1,7 @@
 const TagsButton = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <span className="cursor-default inline-block px-2 py-1 border-2 border-primary text-primary rounded-xl hover:bg-primary hover:text-background">
+      <span className="cursor-default inline-block px-2 py-1 border-2 border-primary text-primary rounded-xl hover:bg-primary hover:text-background whitespace-nowrap">
         {children}
       </span>
     </div>
